99f112147dca777580fd4887fc79c0a183f7ad26
Josep Virgili Llop <jvirgili@nps.edu>
2018-10-19 16:35:33 -0700

    Added a script to install ROS. ROS is initially installed to meet some dependencies that are required by the build_install_debians.sh script.

--- Astrobee_install.sh
+++ Astrobee_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+#Install ROS
+./ROS_install.sh
+
 #Variables
 export SOURCE_PATH=$HOME/Documents/freeflyer
 export ANDROID_PATH="${SOURCE_PATH}_android"
@@ -7,7 +11,7 @@ export INSTALL_PATH="${SOURCE_PATH}_install/native"
 export BUILD_PATH="${SOURCE_PATH}_build/native"
 
 #Copy source code
-sudo apt-get install build-essential git
+sudo apt-get -y install build-essential git
 git clone https://github.com/nasa/astrobee.git $SOURCE_PATH
 git clone https://github.com/nasa/astrobee_android.git $ANDROID_PATH
 
@@ -19,11 +23,7 @@ cd debians
 ./build_install_debians.sh
 cd ../
 ./install_desktop_16_04_packages.sh
-sudo rosdep init
 rosdep update
-
-#Fix permissions just in case
-sudo rosdep fix-permissions
 popd
 
 #Configure build
@@ -32,7 +32,7 @@ pushd $SOURCE_PATH
 popd
 
 #Install a package that is apparently missing
-sudo apt-get install libignition-math2-dev 
+sudo apt-get -y install libignition-math2-dev 
 
 #Building the code
 pushd $BUILD_PATH
